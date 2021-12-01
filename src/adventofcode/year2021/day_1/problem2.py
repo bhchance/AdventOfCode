69688ecba42b2f921eb29611eaca83e32e027d45
Bryan Chance <bryan.chance@amount.com>
2021-12-01 10:55:21 -0600

    Changing implementation to use filter

--- src/adventofcode/year2021/day_1/problem2.py
+++ src/adventofcode/year2021/day_1/problem2.py
@@ -1,7 +1,7 @@
 def solution(input_string):
     nums = list(map(int, input_string.split()))
     chunks_summed = list(map(sum, zip(nums[:-2], nums[1:-1], nums[2:])))
-    return sum(1 for x, y in zip(chunks_summed[:-1], chunks_summed[1:]) if y > x)
+    return sum(1 for _ in filter(lambda xs: xs[1] > xs[0], zip(chunks_summed[:-1], chunks_summed[1:])))
 
 
 if __name__ == "__main__":
