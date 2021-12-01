18bb5d57e53524a5320959c40c0271e4c4ba09ad
Bryan Chance <bryan.chance@amount.com>
2021-12-01 00:00:16 -0600

    Changing implementation to use zip

--- src/adventofcode/year2021/day_1/problem1.py
+++ src/adventofcode/year2021/day_1/problem1.py
@@ -1,6 +1,6 @@
 def solution(input_string):
     nums = list(map(int, input_string.split()))
-    return sum(1 for x in range(len(nums[:-1])) if nums[x + 1] > nums[x])
+    return sum(1 for x, y in zip(nums[:-1], nums[1:]) if y > x)
 
 
 if __name__ == "__main__":
