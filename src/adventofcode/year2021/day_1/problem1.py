ddda738c14407ad289d64ad52185e709aac29e0f
Bryan Chance <bryan.chance@amount.com>
2021-12-01 10:53:13 -0600

    Formatting

--- src/adventofcode/year2021/day_1/problem1.py
+++ src/adventofcode/year2021/day_1/problem1.py
@@ -2,6 +2,7 @@ def solution(input_string):
     nums = list(map(int, input_string.split()))
     return sum(1 for _ in filter(lambda xs: xs[1] > xs[0], zip(nums[:-1], nums[1:])))
 
+
 if __name__ == "__main__":
     with open("input.txt") as f:
         print(solution(f.read().strip()))
