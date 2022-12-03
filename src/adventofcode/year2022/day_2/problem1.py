d620456acd5156a9ce49e68fcf03c2c7f150f4d1
jacquardlabs <66028282+jacquardlabs@users.noreply.github.com>
2022-12-02 20:21:48 -0600

    Day 5 - Part 2

---
+++ src/adventofcode/year2022/day_2/problem1.py
@@ -0,0 +1,28 @@
+import aocd
+
+
+def solution(input_string):
+    return sum({
+                   # losses
+                   "A Z": 0 + 3,
+                   "B X": 0 + 1,
+                   "C Y": 0 + 2,
+                   # ties
+                   "A X": 3 + 1,
+                   "B Y": 3 + 2,
+                   "C Z": 3 + 3,
+                   # wins
+                   "A Y": 6 + 2,
+                   "B Z": 6 + 3,
+                   "C X": 6 + 1,
+               }.get(line) for line in input_string.splitlines())
+
+
+if __name__ == "__main__":
+    year, day, part = 2022, 2, 1
+submit = True
+answer = solution(aocd.get_data())
+
+if submit:
+    aocd.submit(answer, year=year, day=day, part=part)
+print(answer)
