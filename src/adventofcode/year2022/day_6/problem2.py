4ab05c2e0f9442f15dba838efcf8d5640dc78d46
jacquardlabs <66028282+jacquardlabs@users.noreply.github.com>
2022-12-05 21:06:31 -0600

    Day 6 - skeleton

---
+++ src/adventofcode/year2022/day_6/problem2.py
@@ -0,0 +1,15 @@
+import aocd
+
+
+def solution(input_string):
+    pass
+
+
+if __name__ == "__main__":
+    year, day, part = 2022, 6, 2
+    submit = False
+    answer = solution(aocd.get_data())
+
+    if submit:
+        aocd.submit(answer, year=year, day=day, part=part)
+    print(answer)
