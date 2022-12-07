e35204ee4e9dbbfbc808206857d266876c8ff3dc
bryan <bryan@vizit.com>
2022-12-07 10:07:15 -0600

    Day 7

---
+++ src/adventofcode/year2022/day_7/problem1.py
@@ -0,0 +1,59 @@
+import dataclasses
+from itertools import groupby
+from typing import List
+
+import aocd
+from attr import attr
+
+
+def solution(input_string):
+    class File:
+        def __init__(self, name, size, parent):
+            self.name = name
+            self.size = size
+            self.parent = parent
+
+    class Directory:
+        def __init__(self, name, parent=None, children=None):
+            self.name = name
+            self.parent = parent
+            self.children = children or []
+
+        @property
+        def size(self):
+            return sum(c.size for c in self.children)
+
+    root, *rest = input_string.splitlines()
+    root = Directory(name="root")
+    current_directory = root
+    for line in rest:
+        if line.startswith("$"):
+            prompt, command, *target = line.split()
+            if command == "cd":
+                if target == [".."]:
+                    current_directory = current_directory.parent
+                else:
+                    current_directory = next(filter(lambda c: c.name == target[0], current_directory.children))
+        else:
+            p1, p2 = line.split()
+            if p1 == "dir":
+                current_directory.children.append(Directory(name=p2, parent=current_directory))
+            else:
+                current_directory.children.append(File(name=p2, size=int(p1), parent=current_directory))
+
+    def count_items(node, limit=100000):
+        dirs = [c for c in node.children if isinstance(c, Directory)]
+        size = node.size if node.size <= limit else 0
+        return size + sum(map(lambda c: count_items(c, limit), dirs))
+
+    return count_items(root)
+
+
+if __name__ == "__main__":
+    year, day, part = 2022, 7, 1
+    submit = True
+    answer = solution(aocd.get_data())
+
+    if submit:
+        aocd.submit(answer, year=year, day=day, part=part)
+    print(answer)
---
+++ src/adventofcode/year2022/day_7/problem2.py
@@ -0,0 +1,59 @@
+from itertools import chain
+
+import aocd
+
+
+def solution(input_string):
+    class File:
+        def __init__(self, name, size, parent):
+            self.name = name
+            self.size = size
+            self.parent = parent
+
+    class Directory:
+        def __init__(self, name, parent=None, children=None):
+            self.name = name
+            self.parent = parent
+            self.children = children or []
+
+        @property
+        def size(self):
+            return sum(c.size for c in self.children)
+
+    root, *rest = input_string.splitlines()
+    root = Directory(name="root")
+    current_directory = root
+    for line in rest:
+        if line.startswith("$"):
+            prompt, command, *target = line.split()
+            if command == "cd":
+                if target == [".."]:
+                    current_directory = current_directory.parent
+                else:
+                    current_directory = next(filter(lambda c: c.name == target[0], current_directory.children))
+        else:
+            p1, p2 = line.split()
+            if p1 == "dir":
+                current_directory.children.append(Directory(name=p2, parent=current_directory))
+            else:
+                current_directory.children.append(File(name=p2, size=int(p1), parent=current_directory))
+
+    def find_items(node, limit):
+        dirs = [c for c in node.children if isinstance(c, Directory)]
+        node = [node] if node.size >= limit else []
+        return node + list(chain.from_iterable(map(lambda c: find_items(c, limit), dirs)))
+
+    free_space = 70000000 - root.size
+    need_to_free = 30000000 - free_space
+
+    return min(find_items(root, need_to_free), key=lambda x: x.size).size
+
+
+if __name__ == "__main__":
+    year, day, part = 2022, 7, 2
+    submit = True
+    answer = solution(aocd.get_data())
+
+    if submit:
+        aocd.submit(answer, year=year, day=day, part=part)
+    print(answer)
