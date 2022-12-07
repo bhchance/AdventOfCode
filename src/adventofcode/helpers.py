6b7db59073c4e283dbc38a2b759b37354d9e66eb
bryan <bryan@vizit.com>
2022-12-07 10:16:21 -0600

    Add sliding window

--- src/adventofcode/helpers.py
+++ src/adventofcode/helpers.py
@@ -1,3 +1,18 @@
+import collections
+from itertools import islice
+
+
 def chunk(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
+
+
+def sliding_window(iterable, n):
+    # sliding_window('ABCDEFG', 4) --> ABCD BCDE CDEF DEFG
+    it = iter(iterable)
+    window = collections.deque(islice(it, n), maxlen=n)
+    if len(window) == n:
+        yield tuple(window)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
