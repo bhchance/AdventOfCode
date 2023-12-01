cc4642a7718ce90515b35c4c691e2961b850b70f
Bryan Chance <bhchance@gmail.com>
2023-12-01 15:40:40 -0600

    Day 1

--- src/adventofcode/year2023/day1.py
+++ src/adventofcode/year2023/day1.py
@@ -3,7 +3,6 @@ import string
 import aocd
 
 
-
 def solution_part1(input_string):
     return sum(int(line[0] + line[-1]) for line in "".join(filter(lambda c: c in string.digits + "\n", input_string)).splitlines())
 
@@ -22,8 +21,8 @@ def solution_part2(input_string):
     }
     line_values = []
     for line in input_string.splitlines():
-        first_finds = {index: item for item in mapping.keys() if (index := line.find(item) != -1)}
-        last_finds = {index: item for item in mapping.keys() if (index := line.rfind(item) != -1)}
+        first_finds = {index: item for item in mapping.keys() if (index := line.find(item)) != -1}
+        last_finds = {index: item for item in mapping.keys() if (index := line.rfind(item)) != -1}
         line_values.append(mapping[first_finds[min(first_finds)]] + mapping[last_finds[max(last_finds)]])
     return sum(map(int, line_values))
 
